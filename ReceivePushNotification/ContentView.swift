4ea9d23f0004986780df64a1f596f59ad7d692cc
ogawa-rni <k.ogawa@r-n-i.jp>
2020-07-11 18:32:40 +0900

    Create PushNotification App

---
+++ ReceivePushNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReceivePushNotification
+//
+//  Created by ogawa on 2020/07/11.
+//  Copyright © 2020 ogawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
